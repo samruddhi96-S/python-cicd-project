22ef32bf4d30d1a6cd6e87a1ffcec9f893e12930
samruddhi96-S <samruddhi96stp@gmail.com>
2026-08-15 21:29:44 +0530

    Fix Python application and tests

--- app.py
+++ app.py
@@ -6,4 +6,7 @@ def multiply(a, b):
     return a * b
 
 
-print("CI/CD Mini Project Started")
+if __name__ == "__main__":
+    print("Python CI/CD Pipeline is running successfully!")
+    print("2 + 3 =", add(2, 3))
+    print("2 × 3 =", multiply(2, 3))
